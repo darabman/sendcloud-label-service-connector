4413c3b6e7a98797bf5313c94dad7e9f12fc3844
Samuel McNamara <skjmcnamara@gmail.com>
2022-06-19 23:31:39 +0200

    Cleared up unecessary usings

--- LabelServiceConnectorApp/NotifyIcon.cs
+++ LabelServiceConnectorApp/NotifyIcon.cs
@@ -1,11 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using Forms = System.Windows.Forms;
-using System.Windows.Input;
 using System.Drawing;
 
 namespace LabelServiceConnector
